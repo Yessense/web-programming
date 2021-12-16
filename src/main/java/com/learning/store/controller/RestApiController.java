95e3e46743bdb5ce93e05955bfe44d56a4c504fc
Yessense <akorchemnyj@gmail.com>
2021-12-16 21:07:34 +0300

    Add link between controller and service

--- src/main/java/com/learning/store/controller/RestApiController.java
+++ src/main/java/com/learning/store/controller/RestApiController.java
@@ -1,48 +1,79 @@
 package com.learning.store.controller;
 
+import com.learning.store.dto.CartDto;
+import com.learning.store.dto.ItemDto;
+import com.learning.store.service.HttpService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 public class RestApiController {
 
-    // ToDo: service
     private HttpService service;
 
+    // Get methods
     @GetMapping("/products")
-    public List<String> getItems() {
-        return;
+    public List<ItemDto> getItems() {
+        return service.getItems();
+    }
+
+    @GetMapping("/carts")
+    public List<CartDto> getCarts() {
+        return service.getCarts();
     }
 
     @GetMapping("/products/{id]")
     public ItemDto getItem(@PathVariable("id") UUID id) {
-        return;
+        return service.getItem(id);
     }
-
-    @PostMapping("/products")
-    public void createItem(@RequestBody) {
-
-    }
-
-
-    @PutMapping("/products/{id}")
-    public void putItem(@RequestBody) {
-
-
-    }
-
-    @DeleteMapping("/products/{id}")
-
-    @GetMapping("/carts")
 
     @GetMapping("/carts/{id}")
+    public CartDto getCart(@PathVariable("id") UUID id) {
+        return service.getCart(id);
+    }
+
+    // Post methods
+    @PostMapping("/products")
+    public void createItem(@RequestBody) {
+        service.createItem();
+    }
+
+    @PostMapping("/baskets")
+    public CartDto createCart() {
+        return service.createCart();
+    }
+
+    // Delete methods
+    @DeleteMapping("/products/{id}")
+    public void deleteItem(@PathVariable("id") UUID id) {
+        service.deleteItem(id);
+    }
 
     @DeleteMapping("/carts/{id}")
+    public void deleteCart(@PathVariable("id") UUID id) {
+        service.deleteCart(id);
+    }
+
+
+    // Put methods
+    @PutMapping("/products/{id}")
+    public void updateItem(@RequestBody) {
+        service.updateItem();
+    }
+
 
     @PutMapping("/carts/{id}/add_item")
+    public void addCartItem(@PathVariable("id") UUID cart,
+                            @RequestBody) {
+        service.addCartItem();
+    }
 
     @PutMapping("/carts/{id}/remove_item")
-}
+    public void removeCartItem(@PathVariable("id") UUID cart,
+                            @RequestBody) {
+        service.removeCartItem();
+    }
--- src/main/java/com/learning/store/service/HttpService.java
+++ src/main/java/com/learning/store/service/HttpService.java
@@ -1,4 +1,35 @@
 package com.learning.store.service;
 
-public class HttpService {
+import com.learning.store.dto.CartDto;
+import com.learning.store.dto.ItemDto;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface HttpService {
+    // Get methods
+    public List<ItemDto> getItems();
+
+    public List<CartDto> getCarts();
+
+    public ItemDto getItem(UUID id);
+
+    public CartDto getCart(UUID id);
+
+    // Post methods
+    public void createItem();
+
+    public CartDto createCart();
+
+    // Delete methods
+    public void deleteItem(UUID id);
+
+    public void deleteCart(UUID id);
+
+    // Put methods
+    public void updateItem();
+
+    public void addCartItem();
+
+    public void removeCartItem();
 }
