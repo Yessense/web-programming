b180a7cc8433827e678d77e46a546e31400dc415
Yessense <akorchemnyj@gmail.com>
2021-12-17 12:09:53 +0300

    Fixed repo code

--- src/main/java/com/learning/store/repository/CartRepository.java
+++ src/main/java/com/learning/store/repository/CartRepository.java
@@ -1,4 +1,13 @@
 package com.learning.store.repository;
 
-public class CartRepository {
+import com.learning.store.entity.Cart;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+
+@Repository
+public interface CartRepository extends CrudRepository<Cart, UUID> {
+
 }
--- src/main/java/com/learning/store/repository/ItemRepository.java
+++ src/main/java/com/learning/store/repository/ItemRepository.java
@@ -1,4 +1,12 @@
 package com.learning.store.repository;
 
-public class ItemRepository {
+import com.learning.store.entity.Item;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ItemRepository extends CrudRepository<Item, UUID> {
+
 }
